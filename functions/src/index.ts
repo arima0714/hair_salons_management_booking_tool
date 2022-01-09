7356e860939b80a35d254af854527af9ca7fc042
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-01-09 22:22:58 +0900

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,29 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
+import * as admin from 'firebase-admin'
+admin.initializeApp();
+
+const db = admin.firestore()
+
+export const createDocument = functions.https.onRequest(async (req, res) => {
+    await db.collection("test").doc("abc").set({a: 123});
+    res.send();
+});
+
+export const updateDocument = functions.https.onRequest(async (req, res) => {
+    await db.collection("test").doc("abc").update({a: 234, b:345});
+    res.send();
+});
+
+export const deleteDocument = functions.https.onRequest(async (req, res) => {
+    await db.collection("test").doc("abc").delete();
+    res.send();
+});
+
+export const getDocument = functions.https.onRequest(async (req, res) => {
+    const data = await (await db.collection("test").doc("abc").get()).data();
+    res.send(data);
+});
 
 const app = express()
 app.get("/", (req, res) => res.status(200).send('Hey there!'))
