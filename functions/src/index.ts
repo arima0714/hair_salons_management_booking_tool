7c6ccf6bed73d4ab39eb4ba0fde6aeb403d69321
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-01-09 21:37:50 +0900

    追加予定の機能を記述

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,3 +4,15 @@ import * as express from 'express'
 const app = express()
 app.get("/", (req, res) => res.status(200).send('Hey there!'))
 exports.app = functions.https.onRequest(app)
+
+// CREATE
+// 予約情報の作成
+
+// READ
+// 予約情報の追加
+
+// UPDATE
+// 予約情報の更新
+
+// DELETE
+// 予約情報の削除
