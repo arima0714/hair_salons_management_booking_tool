878a100400c14ba726ced7c7a209e7f6031794a1
arima0714 <32933855+arima0714@users.noreply.github.com>
2022-01-11 09:51:10 +0900

    コメントを移動

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,7 +14,8 @@ const db = admin.firestore()
 //     term5: boolean;
 // }
 
-
+// CREATE
+// 予約情報の作成
 export const createDocument = functions.https.onRequest(async (req, res) => {
     interface bookingInfo {
         date: string;
@@ -49,29 +50,23 @@ export const createDocument = functions.https.onRequest(async (req, res) => {
     res.status(200).json(bookingInformation).send();
 });
 
+// UPDATE
+// 予約情報の更新
 export const updateDocument = functions.https.onRequest(async (req, res) => {
     await db.collection("test").doc("abc").update({a: 234, b:345});
     res.send();
 });
 
+// DELETE
+// 予約情報の削除
 export const deleteDocument = functions.https.onRequest(async (req, res) => {
     await db.collection("test").doc("abc").delete();
     res.send();
 });
 
+// READ
+// 予約情報の追加
 export const getDocument = functions.https.onRequest(async (req, res) => {
     const data = await (await db.collection("test").doc("abc").get()).data();
     res.send(data);
 });
-
-// CREATE
-// 予約情報の作成
-
-// READ
-// 予約情報の追加
-
-// UPDATE
-// 予約情報の更新
-
-// DELETE
-// 予約情報の削除
